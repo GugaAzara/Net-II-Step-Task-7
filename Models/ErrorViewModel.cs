705941f4e3f3c848b44d2bde1e93fc30871088fe
GugaAzara <122464022+GugaAzara@users.noreply.github.com>
2023-03-20 14:01:05 +0400

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Net_II_Step_Task_7.Models
-{
-	public class ErrorViewModel
-	{
-		public string? RequestId { get; set; }
-
-		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-	}
-}
